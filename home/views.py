2afda8b6c00e13d70532eb4895b4cfd3ba67e44b
장준혁 <ggogono2@gmail.com>
2022-10-24 11:36:10 +0900

    home

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 def home(request):
-    return render(request, 'home/home.html')
+    context = {"classInfoList": Class.objects.all() }
+    return render(request, 'home/home.html', context)
